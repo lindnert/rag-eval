5c082bd991dddfb63b90b2bec2c98fc3278599fa
lindnert <tim.lindner09@gmail.com>
2026-03-31 14:31:49 +0200

    feat: add fake external docs for retrieval

---
+++ retrieval/docs.py
@@ -0,0 +1,7 @@
+DEFAULT_DOCUMENTS = [
+    "Adults should consume around 0.8g protein per kg body weight.",
+    "Für den Muskelaufbau sind 1,6 bis 2,2 Gramm Protein pro Kilogramm Körpergewicht pro Tag empfehlenswert.",
+    "Kohlenhydrate liefern Energie für Training und Regeneration. Gute Quellen sind Vollkornprodukte, Reis und Kartoffeln.",
+    "Gesunde Fette unterstützen die Hormonproduktion; Avocado, Nüsse und Olivenöl gehören dazu.",
+    "Mikronährstoffe wie Vitamin D, Magnesium und Zink sind wichtig für Muskelregeneration und Gesundheit.",
+]
