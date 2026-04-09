c2031f95f782756545e152c3f65892d004a0b27b
lindnert <tim.lindner09@gmail.com>
2026-04-09 18:11:03 +0200

    fix: path error

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -13,7 +13,7 @@ OLLAMA_RAG_MODEL = os.getenv("OLLAMA_RAG_MODEL", "gemma4:e2b")
 OLLAMA_EVAL_MODEL = os.getenv("OLLAMA_EVAL_MODEL", "qwen3.5:2b")
 OLLAMA_EMBEDDING_MODEL = os.getenv("OLLAMA_EMBEDDING_MODEL", "nomic-embed-text")
 OLLAMA_CONTEXT_LENGTH = int(os.getenv("OLLAMA_CONTEXT_LENGTH", "110000"))
-FAISS_INDEX_DIR = os.path.join(os.path.dirname(__file__), "retrieval", "richtlinien", "faiss_index")
+FAISS_INDEX_DIR = os.path.join(os.path.dirname(__file__), "richtlinien", "faiss_index")
 
 _retriever = None
 
